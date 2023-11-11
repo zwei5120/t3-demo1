7c3f65ad223f103273f9da6b74b62fa3085e08b0
张文建 <zhangwenjian@ouchteam.com>
2023-11-11 19:15:36 +0800

    fix: database.

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -3,7 +3,21 @@ import { api } from "@/utils/api";
 
 const User = () => {
   const data = api.post.hello.useQuery({ text: "user" });
-  return <div>{data?.data?.greeting ?? ""}</div>;
+  const { mutate } = api.post.create.useMutation();
+  const latestData = api.post.getLatest.useQuery();
+  return (
+    <div>
+      <div>{data?.data?.greeting ?? ""}</div>
+      <div>latest data: {latestData?.data?.name ?? ""}</div>
+      <button
+        onClick={() => {
+          mutate({ name: `custom name ${Math.random() * 100}` });
+        }}
+      >
+        create
+      </button>
+    </div>
+  );
 };
 
 export default User;
